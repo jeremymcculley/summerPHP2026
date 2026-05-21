3c42cd5cede8834bd93d0f0114496d85d06e3b9b
jeremymcculley <50459396+jeremymcculley@users.noreply.github.com>
2026-05-21 19:14:16 -0400

    Add files via upload

---
+++ week-3/config.php
@@ -0,0 +1,6 @@
+<?php 
+  // this file will define our API information
+  define("TMDB_API_KEY", "4a28d32dbf05e1734e6a13e084f83736");
+  // now add a base url for TMDB
+  define("TMDB_BASE_URL", "https://api.themoviedb.org/3");
+?>
